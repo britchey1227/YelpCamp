eedb8b6066829fa54e116963efa51826b155c15c
britchey <50119704+britchey1227@users.noreply.github.com>
2024-08-13 22:50:18 -0700

    refactored code

--- app.js
+++ app.js
@@ -154,6 +154,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(8080, () => {
-  console.log("Serving on port 8080");
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
